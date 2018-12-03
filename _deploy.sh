fb383d30ded3a3a23b8e4a87f39291cdd93bf385
Andres Ladino <aladinoster@gmail.com>
2018-12-03 21:20:10 +0100

    Update the book

--- _deploy.sh
+++ _deploy.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+#[ -z "${GITHUB_PAT}" ] && exit 0
+#[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "aladinoster@gmail.com"
-git config --global user.name "Andres Ladino"
+#git config --global user.email "aladinoster@gmail.com"
+#git config --global user.name "Andres Ladino"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
-cp -r ../_book/* ./
+#git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+#cd book-output
+#cp -r ../_book/* ./
+cp -r _book/* ./
 git add --all *
 git commit -m"Update the book" || true
 git push -q origin gh-pages
