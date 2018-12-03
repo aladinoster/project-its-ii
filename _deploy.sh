12bcb525774da3a2673dc31effb7514000960a29
Andres Ladino <aladinoster@gmail.com>
2018-12-03 21:01:12 +0100

    Rollback Test 2

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-#[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "aladinoster@gmail.com"
 git config --global user.name "Andres Ladino"
